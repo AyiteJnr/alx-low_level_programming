cde8b3e256231e9edcd63e71ed0f8f46d8fd0bdd
Seth Ayi-Bonte <sethayi433@gmail.com>
2023-07-17 07:55:51 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,8 @@
  */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
-printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
